40f52f7747326e54339da9d51371c72a1e6d2602
Asror Zakirov <asror.zk@gmail.com>
2021-09-28 10:51:37 +0500

    1333333

---
+++ Fluxcd Flux2 - Cmdline.go
@@ -0,0 +1,2 @@
+d:\Develop\CI-CDs\GitOps\Fluxcd Flux2\Cmdline
+\Develop\ \Fluxcd Flux2\ \Cmdline
--- flux bootstrap github.cmd
+++ flux bootstrap github.cmd
@@ -2,6 +2,8 @@
 
 chcp 65001
 
-setx GITHUB_TOKEN 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
+set GITHUB_TOKEN=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
+set GITHUB_USER=asror.zk@gmail.com
 
-flux bootstrap github --private=false --owner=teamprodev --repository=kubernetes_shipox --path "./clusters/eks-ha" --interval "1m0s" --branch "main"
+REM flux bootstrap github --private=false --owner=teamprodev --repository=kubernetes_shipox --path "./clusters/eks-ha" --interval "1m0s" --branch "master" --token-auth
+flux bootstrap github --private=false --owner=teamprodev --repository=kubernetes_shipox --path "/" --interval "1m0s" --branch "master" --token-auth
